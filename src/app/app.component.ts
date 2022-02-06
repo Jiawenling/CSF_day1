94a194ae4b4041b30baf8205b88a27830cfdd576
Jiawenling <jling002@e.ntu.edu.sg>
2022-02-06 12:13:12 +0800

    working on sending Person object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {StringConcatBuiltinFn} from "@angular/compiler-cli/src/ngtsc/partial_evaluator/src/builtin";
-import {Form, FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Person, BioData} from './person'
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {DialogComponent} from "./dialog.component";
-// @ts-ignore
-import {v4 as uuidv4} from 'uuid';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {RegisterComponent} from "./register.component";
+
 
 @Component({
   selector: 'app-root',
@@ -27,81 +22,16 @@ export class AppComponent implements OnInit{
   // myNumber:number = 0;
   // numberForChild2 = 0;
 
-  personalForm!:FormGroup;
-  newFormArray!: FormArray;
-  contactOptions = ['Phone', 'SMS', 'Email']
-  bioInfo: FormArray = this.fb.array([])
 
-
-  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+  constructor() {
   };
 
   ngOnInit() {
-    this.personalForm = this.createForm()
+
   }
 
-  createFormArray(): FormArray {
-    this.newFormArray = this.fb.array([])
-    for(let i = 0; i< this.contactOptions.length; i++){
-        this.newFormArray.push(this.fb.control(''))
-    }
-    return this.newFormArray
-  }
+  @ViewChild(RegisterComponent)
+  registerComponent!:RegisterComponent
 
-  createForm(){
-    return this.fb.group({
-      name: this.fb.control('Perry', [Validators.required, Validators.minLength(3)]),
-      phone: this.fb.control('', [Validators.required, Validators.minLength(3)]),
-      email: this.fb.control('', [Validators.email, Validators.required]),
-      gender: this.fb.control('', Validators.required),
-      contactOption: this.createFormArray(),
-      bioInfo: this.bioInfo
-    })
-  }
-
-  addBio(){
-    let bioGroup = this.fb.group({
-      traits: this.fb.control('', Validators.required),
-      description: this.fb.control('', Validators.required)
-    })
-    this.bioInfo.push(bioGroup)
-  }
-
-  deleteBioOption(index: number){
-    if(this.bioInfo.length==1){
-      this.openDialog(index)
-    } else{
-      this.deleteBio(index)
-    }
-  }
-
-  deleteBio(index:number){
-    this.bioInfo.removeAt(index)
-  }
-
-  bioIsEmpty(){
-    return this.bioInfo.length==0
-  }
-
-  processForm(){
-    console.log(this.personalForm.value)
-    const person = this.personalForm.value as Person
-    person.userId =uuidv4()
-    person.contactOption =person.contactOption.map(v => !!v)
-    console.log(person)
-    localStorage.setItem(person.userId, JSON.stringify(person))
-    this.personalForm.reset()
-  }
-
-  openDialog(i:number): void {
-    const dialogRef = this.dialog.open(DialogComponent, {
-      width: '250px'
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if(result.data==='true'){
-        this.deleteBio(i)
-      }
-    });
-  }
 }
 
--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {Person} from "./person";
+import {DialogComponent} from "./dialog.component";
+// @ts-ignore
+import {v4 as uuidv4} from 'uuid';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  personalForm!:FormGroup;
+  newFormArray!: FormArray;
+  contactOptions = ['Phone', 'SMS', 'Email']
+  bioInfo: FormArray = this.fb.array([])
 
-  ngOnInit(): void {
+
+  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+  };
+
+  ngOnInit() {
+    this.personalForm = this.createForm()
+  }
+
+  createFormArray(): FormArray {
+    this.newFormArray = this.fb.array([])
+    for(let i = 0; i< this.contactOptions.length; i++){
+      this.newFormArray.push(this.fb.control(''))
+    }
+    return this.newFormArray
+  }
+
+  createForm(){
+    return this.fb.group({
+      name: this.fb.control('Perry', [Validators.required, Validators.minLength(3)]),
+      phone: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+      email: this.fb.control('', [Validators.email, Validators.required]),
+      gender: this.fb.control('', Validators.required),
+      contactOption: this.createFormArray(),
+      bioInfo: this.createBioArray()
+    })
+  }
+
+  createBioGroup(){
+    return this.fb.group({
+      traits: this.fb.control('', Validators.required),
+      description: this.fb.control('', Validators.required)
+    })
+  }
+
+  addBio(){
+    this.bioInfo.push(this.createBioGroup())
+  }
+
+  createBioArray(){
+    this.addBio()
+    return this.bioInfo
+  }
+
+
+
+  deleteBioOption(index: number){
+    if(this.bioInfo.length==1){
+      this.openDialog(index)
+    } else{
+      this.deleteBio(index)
+    }
+  }
+
+  deleteBio(index:number){
+    this.bioInfo.removeAt(index)
+  }
+
+  bioIsEmpty(){
+    return this.bioInfo.length==0
+  }
+
+  processForm(){
+    console.log(this.personalForm.value)
+    const person = this.personalForm.value as Person
+    person.userId =uuidv4()
+    person.contactOption =person.contactOption.map(v => !!v)
+    console.log(person)
+    localStorage.setItem(person.userId, JSON.stringify(person))
+    this.personalForm.reset()
+  }
+
+  openDialog(i:number): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '250px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.data==='true'){
+        this.deleteBio(i)
+      }
+    });
   }
 
 }
